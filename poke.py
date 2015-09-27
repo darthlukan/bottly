ab4b40f316c4f3a9cbd42793621f246c021a4ac3
dragonkeeper <ryan@dropoffpoint.co.uk>
2015-09-27 12:36:26 +0100

    .

--- poke.py
+++ poke.py
@@ -1,7 +1,17 @@
 ############ ###user # add pokemon ##
     if self.trigger + 'gamer' == command:
-        gamer = self.gamer(sender, message)
-        self.send_message(channel, sender, '.. You Choose', message)
+        if self.is_admin(sender):
+            try:
+                channel = data[4]
+            except IndexError:
+                pass
+            gamer = self.gamer(sender, message)
+            self.send_message(channel, sender, '.. You Choose', message)
+        else:
+            self.deny_command(channel, sender)
+
+
+
     def gamer(sender, message):
         db = self.db
         database.save_gamer(db, sender, message)
