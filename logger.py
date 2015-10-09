78701eb9f0c1dbbf3a2dc86875146ab6700d9774
nixheads <ryan@nixheads.co.uk>
2015-10-09 03:06:58 +0100

    added logfile check and add
    caused no file found error

--- logger.py
+++ logger.py
@@ -6,7 +6,10 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 def log_setup():
     logger = logging.getLogger('myapp')
-    hdlr = logging.FileHandler(base_dir + "/log/command.log")
+    if not os.path.exists(base_dir + "/log/"):
+        os.makedirs(base_dir + "/log/")
+        if not os.path.exists(base_dir + "/log/command.log"):
+            hdlr = logging.FileHandler(base_dir + "/log/command.log", 'w')
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     hdlr.setFormatter(formatter)
     logger.addHandler(hdlr) 
