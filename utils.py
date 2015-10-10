83c4f7ccc54fcca1dc471ac91dd937073930962d
Brian Tomlinson <darthlukan@gmail.com>
2015-10-10 23:53:43 +0200

    massive cleanup: split out a bunch of operations to their own methods, cleaned out unnecessary comments, cleaned up formatting, added gitignore entries for pyc's and linter

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def unicode_to_bytes(data):
     return data.encode("UTF-8")
 
 
-def pretty_print(self, user, msg_type, destination, message):
+def pretty_print(user, msg_type, destination, message):
     if isinstance(message, list):
         message = " ".join(message)
     print("%s %s %s :%s" % (user, msg_type, destination, message))
